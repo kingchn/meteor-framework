7a9bf738cb3454a75cf157a115e14218905c092a
kingchn <shenjc@qq.com>
2017-06-02 13:26:03 +0800

    二维码工具类新增encodeQRCodeToBase64String方法

--- util/src/main/java/cn/meteor/module/util/qrcode/QRCodeUtils.java
+++ util/src/main/java/cn/meteor/module/util/qrcode/QRCodeUtils.java
@@ -2,6 +2,7 @@ package cn.meteor.module.util.qrcode;
 
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -19,14 +20,17 @@ import org.apache.commons.codec.binary.Base64;
 
 import com.google.zxing.BinaryBitmap;
 import com.google.zxing.DecodeHintType;
+import com.google.zxing.EncodeHintType;
 import com.google.zxing.LuminanceSource;
 import com.google.zxing.MultiFormatReader;
 import com.google.zxing.NotFoundException;
 import com.google.zxing.Result;
+import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.common.HybridBinarizer;
 import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 public class QRCodeUtils {
 	
@@ -65,6 +69,62 @@ public class QRCodeUtils {
 			System.out.println(e.getMessage());
 		}
 	}
+	
+	public static String encodeQRCodeToBase64String(String text, int width, int height) throws IOException, WriterException {
+//		Charset charset = Charset.forName("UTF-8");
+//		CharsetEncoder encoder = charset.newEncoder();
+//		byte[] bytes = null;
+//		// Convert a string to ISO-8859-1 bytes in a ByteBuffer
+//		ByteBuffer byteBuffer = encoder.encode(CharBuffer.wrap(text));
+//		bytes = byteBuffer.array();
+//		
+//		String data = new String(bytes, "iso8859-1");
+		com.google.zxing.Writer writer = new QRCodeWriter();
+		Hashtable<EncodeHintType, Object> hints = new Hashtable<EncodeHintType, Object>();
+//        hints.put(EncodeHintType.CHARACTER_SET, "utf-8");
+//        hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
+        hints.put(EncodeHintType.MARGIN, 1);
+		BitMatrix matrix = writer.encode(text, com.google.zxing.BarcodeFormat.QR_CODE, width, height, hints); // get a byte matrix for the data
+		
+//		matrix = deleteWhite(matrix);//删除白边
+//		
+//		width = matrix.getWidth();
+//        height = matrix.getHeight();
+//        int[] pixels = new int[width * height];
+//        for (int y = 0; y < height; y++) {
+//            for (int x = 0; x < width; x++) {
+//                if (matrix.get(x, y)) {
+//                    pixels[y * width + x] = Color.BLACK.getRGB();
+//                } else {
+//                    pixels[y * width + x] = Color.WHITE.getRGB();
+//                }
+//            }
+//        }
+//        BinaryBitmap bitmap = BinaryBitmap.createBitmap(width, height, BinaryBitmap.Config.ARGB_8888);
+//        bitmap..setPixels(pixels, 0, width, 0, 0, width, height);
+		
+		BufferedImage bufferedImage = MatrixToImageWriter.toBufferedImage(matrix);
+		ByteArrayOutputStream out = new ByteArrayOutputStream();  
+		boolean flag = ImageIO.write(bufferedImage, "jpg", out);
+        byte[] qrCodeBytes = out.toByteArray();
+		return Base64.encodeBase64String(qrCodeBytes);
+	}
+	
+//	private static BitMatrix deleteWhite(BitMatrix matrix) {
+//        int[] rec = matrix.getEnclosingRectangle();
+//        int resWidth = rec[2] + 1;
+//        int resHeight = rec[3] + 1;
+//
+//        BitMatrix resMatrix = new BitMatrix(resWidth, resHeight);
+//        resMatrix.clear();
+//        for (int i = 0; i < resWidth; i++) {
+//            for (int j = 0; j < resHeight; j++) {
+//                if (matrix.get(i + rec[0], j + rec[1]))
+//                    resMatrix.set(i, j);
+//            }
+//        }
+//        return resMatrix;
+//    }
 	
 	public static String decodeQRCodeBase64StringToString(String qrCodeBase64String) throws IOException, NotFoundException {
 		Result result = decodeQRCodeBase64String(qrCodeBase64String);
--- util/src/test/java/cn/meteor/module/util/qrcode/QRCodeUtilsTest.java
+++ util/src/test/java/cn/meteor/module/util/qrcode/QRCodeUtilsTest.java
@@ -7,17 +7,37 @@ import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 
 import com.google.zxing.NotFoundException;
+import com.google.zxing.WriterException;
 
 public class QRCodeUtilsTest {
 
 	private  static final Logger logger = LogManager.getLogger(QRCodeUtilsTest.class);
+	
+	@Test
+	public void encodeQRCodeToBase64String() {
+		String text = "01,51,044001600211,98580990,149.60,20170515,80692991863912413132,4BE5";
+		int width=75;
+		int height=75;
+//		int width=100;
+//		int height=100;
+		try {
+			String qrCodeBase64String = QRCodeUtils.encodeQRCodeToBase64String(text, width, height);
+			System.out.println(qrCodeBase64String);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (WriterException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 	@Test
 	public void testDecodeQRCodeBase64StringToString() {
 		String qrCodeBase64String ="Qk3CAwAAAAAAAD4AAAAoAAAASwAAAEsAAAABAAEAAAAAAIQDAAAAAAAAAAAAAAAAAAACAAAAAAAA///////////////////gAAAAAwM/A8/z/8AgAAAAAwM/A8/z/8AgAAA/8/DAAA/z/8DgAAA/8/DAAA/z/8DgAAAwM8A8zMA8M/PgAAAwM8A8zMA8M/PgAAAwM/D/8PA88/MgAAAwM/D/8PA88/MgAAAwMwAPPDzzADMgAAAwMwAPPDzzADMgAAA/8/AD/wzwPwPgAAA/8/AD/wzwPwPgAAAAAwPA88D/MzDgAAAAAwPA88D/MzDgAAD//wPAzzAMPzzgAAD//wPAzzAMPzzgAADw88zAAM/zAAAgAADw88zAAM/zAAAgAAAAz8Mzz8P/D8PgAAAAz8Mzz8P/D8PgAAADAMPDDwzwzwzgAAADAMPDDwzwzwzgAADwPPz/PAw8zAzgAADwPPz/PAw8zAzgAADP8MwD8/MzM8AgAADP8MwD8/MzM8AgAAAzPzMwA/Azz8PgAAAzPzMwA/Azz8PgAAA/MDPADzwMA/DgAAA/MDPADzwMA/DgAAAAzwww/wwAzA/gAAAAzwww/wwAzA/gAADwA///AwzwM8MgAADwA///AwzwM8MgAAAwzDzMDAAwDwDgAAAwzDzMDAAwDwDgAADw88P8/MD8zMDgAADw88P8/MD8zMDgAAA8z//D8PwMzDzgAAA8z//D8PwMzDzgAAAz8ADPPzzzM8DgAAAz8ADPPzzzM8DgAAAMz8/8/AzwPMDgAAAMz8/8/AzwPMDgAADDMDAA88z8D8DgAADDMDAA88z8D8DgAAAwPPwwzzAMzADgAAAwPPwwzzAMzADgAADMAw8wAM/zM8MgAADMAw8wAM/zM8MgAAAwPPwPz8wwDwPgAAAwPPwPz8wwDwPgAADMwAz/AwD8/MDgAADMwAz/AwD8/MDgAADzDAA/DAPMzDPgAADzDAA/DAPMzDPgAAA/MAAz8M8zAA8gAAA/MAAz8M8zAA8gAAD//zMwwAz8P//gAAD//zMwwAz8P//gAAAAAzMzMzMzMAAgAAAAAzMzMzMzMAAgAAA/8zwP8DMP8/8gAAA/8zwP8DMP8/8gAAAwMw888zAM8wMgAAAwMw888zAM8wMgAAAwMwz8/DAM8wMgAAAwMwz8/DAM8wMgAAAwM8P8zA//MwMgAAAwM8P8zA//MwMgAAA/88MAMz8z8/8gAAA/88MAMz8z8/8gAAAAAzzA8M/M8AAgAAAAAzzA8M/M8AAgAAA=";
 		try {
 			String content = QRCodeUtils.decodeQRCodeBase64StringToString(qrCodeBase64String);
-			logger.info(content);
+			System.out.println(content);
 		} catch (NotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
