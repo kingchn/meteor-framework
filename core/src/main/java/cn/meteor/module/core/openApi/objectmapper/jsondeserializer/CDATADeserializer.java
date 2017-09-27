5189c4d757c4276afd92cf5fa9c5f42e85dedb77
kingchn <shenjc@qq.com>
2017-09-27 17:21:02 +0800

    新增当CDATADeserializer如果反序列化时，尝试设置到该实体的content字段中，以便再别处进行解码

--- core/src/main/java/cn/meteor/module/core/openApi/objectmapper/jsondeserializer/CDATADeserializer.java
+++ core/src/main/java/cn/meteor/module/core/openApi/objectmapper/jsondeserializer/CDATADeserializer.java
@@ -1,9 +1,12 @@
 package cn.meteor.module.core.openApi.objectmapper.jsondeserializer;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.converter.json.Jackson2ObjectMapperFactoryBean;
 
 import com.fasterxml.jackson.core.JsonParser;
@@ -14,6 +17,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 public class CDATADeserializer<T> extends JsonDeserializer<T> {
+	
+	private static final Logger logger = LoggerFactory.getLogger(CDATADeserializer.class);
 
 	@Override
 	public T deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JsonProcessingException {
@@ -32,8 +37,21 @@ public class CDATADeserializer<T> extends JsonDeserializer<T> {
         Class <T> entityClass = (Class<T>) types[0];
 //        System.out.println(entityClass.getSimpleName());		
 //		Class <T> entityClass = (Class <T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
-		T t = objectMapper.readValue(content, entityClass);
-		return t;
+		try {
+			T t = objectMapper.readValue(content, entityClass);
+			return t;
+		} catch (Exception e) {//当CDATADeserializer如果反序列化时，尝试设置到该实体的content字段中，以便再别处进行解码
+			T bean = null;
+			try {
+				bean = entityClass.newInstance();
+				Method translateFieldNameMethod = entityClass.getDeclaredMethod("setContent", String.class);
+				translateFieldNameMethod.invoke(bean, content);//调用该类的字段名称转换方法得到字段名称
+				return bean;
+			} catch (Exception e1) {
+				logger.error("执行CDATADeserializer setContent时出错", e1);
+			}
+			return bean;
+		}
 	}
 
 }
