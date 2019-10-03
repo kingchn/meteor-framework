505d06dfb69f2f02922500d5f37089e735c79cb7
kingchn <shenjc@qq.com>
2019-10-03 11:06:12 +0800

    新增文件工具类，支持文件简单读写

--- util/src/main/java/cn/meteor/module/util/file/FileUtils.java
+++ util/src/main/java/cn/meteor/module/util/file/FileUtils.java
@@ -1,9 +1,21 @@
-package cn.meteor.module.util.security.sm.test.util;
+package cn.meteor.module.util.file;
 
 import java.io.IOException;
 import java.io.RandomAccessFile;
 
-public class FileUtil {
+/**
+ * 文件工具类
+ * @author shenjc
+ *
+ */
+public class FileUtils {
+	
+    /**
+     * 将字节数组数据写入到指定路径文件
+     * @param filePath 指定文件路径
+     * @param data 字节数组数据
+     * @throws IOException
+     */
     public static void writeFile(String filePath, byte[] data) throws IOException {
         RandomAccessFile raf = null;
         try {
@@ -16,6 +28,12 @@ public class FileUtil {
         }
     }
 
+    /**
+     * 读取指定路径文件得到字节数组数据
+     * @param filePath 指定文件路径
+     * @return 字节数组数据
+     * @throws IOException
+     */
     public static byte[] readFile(String filePath) throws IOException {
         RandomAccessFile raf = null;
         byte[] data;
@@ -30,4 +48,5 @@ public class FileUtil {
             }
         }
     }
+    
 }
